1ec75a177f001e6abc3fa3c58f7e10f2b8a38b23
June Pyle <github@ruleofsix.ca>
2025-11-07 18:47:43 -0800

    initialize project

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+
+/* Function that creates a new window and loads index.html into it */
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+/* Load GUI when app is ready ('ready' is an event that app fires */
+app.whenReady().then(() => {
+  createWindow();
+});
+
+/* Quit app when window is closed */
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
