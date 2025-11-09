f353eb1794084d87176271ed9f9989930b02b4e1
June Pyle <github@ruleofsix.ca>
2025-11-08 20:37:27 -0800

    FIX NONSENSE

--- renderer/js/new-template-renderer.js
+++ renderer/js/new-template-renderer.js
@@ -1,14 +1,19 @@
 import { displayTemplateStructure } from "./treeRendering.js";
 const templates = await window.templit.templates();
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  document.getElementById("template-location-btn").addEventListener("click", getTemplatePathToCopy);
-  document.getElementById("cancel-btn").addEventListener("click", closeWindow);
-  document.getElementById("save-template-btn").addEventListener("click", saveTemplate);
-});
+document
+  .getElementById("template-location-btn")
+  .addEventListener("click", getTemplatePathToCopy);
+document
+  .getElementById("cancel-template-btn")
+  .addEventListener("click", closeWindow);
+document
+  .getElementById("save-template-btn")
+  .addEventListener("click", saveTemplate);
 
 async function getTemplatePathToCopy() {
   try {
+    console.log("sup");
     let dialogObj = await window.functions.getTemplatePathToCopy();
     let localPathName = dialogObj.filePaths[0];
     const fileStructure = window.templit.filePathToArray(localPathName);
@@ -18,6 +23,7 @@ async function getTemplatePathToCopy() {
     );
     window.functions.sendCopyTemplateFilePath(localPathName);
   } catch (err) {
+    console.log("sup");
     console.log(err);
   }
 }
@@ -28,7 +34,6 @@ function closeWindow() {
 
 function saveTemplate() {
   // const templateSourcePath = document.getElementById("template-location").value;
-  const templateSourcePath = "C:\\Users\\misch\\Downloads\\assignment1";
   const templateName = document.getElementById("template-name").value;
   window.templit.createNewTemplate(templateSourcePath, templateName);
   window.templit.refreshWindow("templateWindow");
